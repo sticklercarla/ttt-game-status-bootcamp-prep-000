5a04a3fb00eea360dd2d37b7907d4ad935138e61
Carla Stickler <carlastickler@gmail.com>
2019-09-18 02:29:51 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -28,7 +28,7 @@ def won?(board)
     position_3 = board[win_index_3]
     if position_1 == "X" && position_2 == "X" && position_3 == "X" 
       return combo
-    elsif position_1 == "0" && position_2 == "O" && position_3 == "0"
+    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
       return combo
     end
   }
