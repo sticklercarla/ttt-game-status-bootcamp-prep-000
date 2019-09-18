a9d459ff901ace5585eb33e63dcdcad0a45f9aad
Carla Stickler <carlastickler@gmail.com>
2019-09-18 01:34:46 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -18,7 +18,7 @@ WIN_COMBINATIONS = [
 ]
 
 def won?(board)
-  for each combo in WIN_COMBINATIONS
+  WIN_COMBINATIONS.each do |combo|
     win_index_1 = combo[0]
     win_index_2 = combo[1]
     win_index_3 = combo[2]
